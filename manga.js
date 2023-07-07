a792bf8d3a003b65841d26b8126542575377d5fd
taiarima <tai.arima@colorado.edu>
2023-07-07 11:23:07 +0300

    Cleaned up html, added manga class.

---
+++ manga.js
@@ -0,0 +1,22 @@
+
+// This class is used for adding a new manga to the map,
+// just to ensure consistency
+class manga {
+    constructor(title, author, year, summary, image, coords, locationName) {
+      this.title = title;
+      this.author = author;
+      this.year = year;
+      this.summary = summary;
+  
+      this.image = image;
+      
+      this.coords = coords;
+      this.locationName = locationName;
+    }
+  }
+  
+  const hoshiNoSamidare = new manga("Lucifer and the Biscuit Hammer", "Satoshi Mizukami", "28 April 2005 - 30 August 2010", "Manga about stuff", "sample_cover.jpg", "28,28", "Tokyo, Japan");
+
+  const mangaList = [];
+
+  mangaList.push(hoshiNoSamidare);
--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 "use strict";
 
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-const form = document.querySelector(".form");
-const containerWorkouts = document.querySelector(".workouts");
-const inputType = document.querySelector(".form__input--type");
-const inputDistance = document.querySelector(".form__input--distance");
-const inputDuration = document.querySelector(".form__input--duration");
-const inputCadence = document.querySelector(".form__input--cadence");
-const inputElevation = document.querySelector(".form__input--elevation");
-
 navigator.geolocation.getCurrentPosition(
   function (position) {
     console.log(position);
@@ -25,7 +15,7 @@ navigator.geolocation.getCurrentPosition(
 
     L.marker(coords)
       .addTo(map)
-      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+      .bindPopup("Manga Here.")
       .openPopup();
   },
   function () {
